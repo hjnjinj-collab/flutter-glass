7ae91d8f6984ed270afaafef8f794d5d05ce1ad1
hjnjinj-collab <hjnjinj@gmail.com>
2026-08-19 16:16:22 +0800

    Add shim to satisfy generated CMake: include plugin implementation into example build

---
+++ plugins/native_glass/example/windows/windows/native_glass.cpp
@@ -0,0 +1,4 @@
+// This file is a small shim so the generated example CMake can compile the plugin source
+// as a relative path. It includes the real implementation from the plugin folder.
+
+#include "../../../windows/native_glass.cpp"
